c3655b7c37b49633a570c73cd04e5fbf59cfd103
James Giddings <james.p.giddings@gmail.com>
2022-09-10 10:36:22 +0100

    Initial commit

---
+++ src/main/java/com/giddings/roguelike/RoguelikeApplication.java
@@ -0,0 +1,13 @@
+package com.giddings.roguelike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoguelikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoguelikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giddings/roguelike/RoguelikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giddings.roguelike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoguelikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
