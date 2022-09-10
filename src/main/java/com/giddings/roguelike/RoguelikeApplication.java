76bbf33f6ed2d91744a22810a6991e6a077042c6
James Giddings <james.p.giddings@gmail.com>
2022-09-10 10:40:36 +0100

    Database Connection and Hello, World

--- src/main/java/com/giddings/roguelike/RoguelikeApplication.java
+++ src/main/java/com/giddings/roguelike/RoguelikeApplication.java
@@ -1,13 +1,20 @@
 package com.giddings.roguelike;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RoguelikeApplication {
+public class RoguelikeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoguelikeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello, World!");
+
 	}
 
 }
